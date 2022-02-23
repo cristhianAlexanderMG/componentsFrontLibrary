0959331f352722545b611a3a6ba3901b0a785280
Cristhian Alexander Mariño <ing.cristhianalexandermg@hotmail.com>
2022-02-23 16:01:49 -0500

    save changes line charts

--- src/app/components/examples/charts-example/charts-example.component.ts
+++ src/app/components/examples/charts-example/charts-example.component.ts
@@ -15,8 +15,17 @@ export class ChartsExampleComponent implements OnInit {
   // Data Char Doughnut
   basicDoughnutData: any;
 
+  // Data Char Line
+  basicLineData: any;
+  multiAxisLineData: any;
+
   // Option Char Doughnut
   chartDoughnutOptions: any;
+
+  // Option Char Line
+  basicLineOptions: any;
+  multiAxisLineOptions: any;
+  lineStylesData: any;
 
   // Option Char Bar
   basicBarOptions: any;
@@ -28,11 +37,127 @@ export class ChartsExampleComponent implements OnInit {
   enumSize = EnumerablesTypesApp.enumSize;
   enumTypeTheme = EnumerablesTypesApp.enumTypeTheme;
   theme = this.enumTypeTheme.dark;
+  labelButtonTheme = "Change to " + this.theme + " Theme";
 
   ngOnInit(): void {
     this.loadDataChartBar();
+    this.loadDataChartLine();
     this.loadDataChartDoughnut();
     this.changesTheme();
+  }
+
+  loadDataChartLine() {
+    this.basicLineData = {
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+      datasets: [
+        {
+          label: 'First Dataset',
+          data: [65, 59, 80, 81, 56, 55, 40],
+          fill: false,
+          borderColor: '#42A5F5',
+          tension: .4
+        },
+        {
+          label: 'Second Dataset',
+          data: [28, 48, 40, 19, 86, 27, 90],
+          fill: false,
+          borderColor: '#FFA726',
+          tension: .4
+        }
+      ]
+    };
+
+    this.multiAxisLineData = {
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+      datasets: [{
+        label: 'Dataset 1',
+        fill: false,
+        borderColor: '#42A5F5',
+        yAxisID: 'y',
+        tension: .4,
+        data: [65, 59, 80, 81, 56, 55, 10]
+      }, {
+        label: 'Dataset 2',
+        fill: false,
+        borderColor: '#00bb7e',
+        yAxisID: 'y1',
+        tension: .4,
+        data: [28, 48, 40, 19, 86, 27, 90]
+      }]
+    };
+
+    this.lineStylesData = {
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+      datasets: [
+        {
+          label: 'First Dataset',
+          data: [65, 59, 80, 81, 56, 55, 40],
+          fill: false,
+          tension: .4,
+          borderColor: '#42A5F5'
+        },
+        {
+          label: 'Second Dataset',
+          data: [28, 48, 40, 19, 86, 27, 90],
+          fill: false,
+          borderDash: [5, 5],
+          tension: .4,
+          borderColor: '#66BB6A'
+        },
+        {
+          label: 'Third Dataset',
+          data: [12, 51, 62, 33, 21, 62, 45],
+          fill: true,
+          borderColor: '#FFA726',
+          tension: .4,
+          backgroundColor: 'rgba(255,167,38,0.2)'
+        }
+      ]
+    };
+
+    this.multiAxisLineOptions = {
+      stacked: false,
+      plugins: {
+        legend: {
+          labels: {
+            color: '#495057'
+          }
+        }
+      },
+      scales: {
+        x: {
+          ticks: {
+            color: '#495057'
+          },
+          grid: {
+            color: '#ebedef'
+          }
+        },
+        y: {
+          type: 'linear',
+          display: true,
+          position: 'left',
+          ticks: {
+            color: '#495057'
+          },
+          grid: {
+            color: '#ebedef'
+          }
+        },
+        y1: {
+          type: 'linear',
+          display: true,
+          position: 'right',
+          ticks: {
+            color: '#495057'
+          },
+          grid: {
+            drawOnChartArea: false,
+            color: '#ebedef'
+          }
+        }
+      }
+    };
   }
 
   loadDataChartDoughnut() {
@@ -241,10 +366,12 @@ export class ChartsExampleComponent implements OnInit {
     if (this.theme == this.enumTypeTheme.dark) {
       this.applyDarkTheme();
       this.theme = this.enumTypeTheme.light;
+      this.labelButtonTheme = "Change to " + this.theme + " Theme";
     }
     else {
       this.applyLightTheme();
       this.theme = this.enumTypeTheme.dark;
+      this.labelButtonTheme = "Change to " + this.theme + " Theme";
     }
   }
 
@@ -400,6 +527,78 @@ export class ChartsExampleComponent implements OnInit {
         }
       }
     };
+
+    this.basicLineOptions = {
+      plugins: {
+        legend: {
+          labels: {
+            color: '#ebedef'
+          }
+        }
+      },
+      scales: {
+        x: {
+          ticks: {
+            color: '#ebedef'
+          },
+          grid: {
+            color: 'rgba(255,255,255,0.2)'
+          }
+        },
+        y: {
+          ticks: {
+            color: '#ebedef'
+          },
+          grid: {
+            color: 'rgba(255,255,255,0.2)'
+          }
+        }
+      }
+    };
+
+    this.multiAxisLineOptions = {
+      stacked: false,
+      plugins: {
+        legend: {
+          labels: {
+            color: '#ebedef'
+          }
+        }
+      },
+      scales: {
+        x: {
+          ticks: {
+            color: '#ebedef'
+          },
+          grid: {
+            color: 'rgba(255,255,255,0.2)'
+          }
+        },
+        y: {
+          type: 'linear',
+          display: true,
+          position: 'left',
+          ticks: {
+            color: '#ebedef'
+          },
+          grid: {
+            color: 'rgba(255,255,255,0.2)'
+          }
+        },
+        y1: {
+          type: 'linear',
+          display: true,
+          position: 'right',
+          ticks: {
+            color: '#ebedef'
+          },
+          grid: {
+            drawOnChartArea: false,
+            color: 'rgba(255,255,255,0.2)'
+          }
+        }
+      }
+    };
   }
 
   applyLightTheme() {
@@ -550,6 +749,78 @@ export class ChartsExampleComponent implements OnInit {
         legend: {
           labels: {
             color: '#495057'
+          }
+        }
+      }
+    };
+
+    this.basicLineOptions = {
+      plugins: {
+        legend: {
+          labels: {
+            color: '#495057'
+          }
+        }
+      },
+      scales: {
+        x: {
+          ticks: {
+            color: '#495057'
+          },
+          grid: {
+            color: '#ebedef'
+          }
+        },
+        y: {
+          ticks: {
+            color: '#495057'
+          },
+          grid: {
+            color: '#ebedef'
+          }
+        }
+      }
+    };
+
+    this.multiAxisLineOptions = {
+      stacked: false,
+      plugins: {
+        legend: {
+          labels: {
+            color: '#495057'
+          }
+        }
+      },
+      scales: {
+        x: {
+          ticks: {
+            color: '#495057'
+          },
+          grid: {
+            color: '#ebedef'
+          }
+        },
+        y: {
+          type: 'linear',
+          display: true,
+          position: 'left',
+          ticks: {
+            color: '#495057'
+          },
+          grid: {
+            color: '#ebedef'
+          }
+        },
+        y1: {
+          type: 'linear',
+          display: true,
+          position: 'right',
+          ticks: {
+            color: '#495057'
+          },
+          grid: {
+            drawOnChartArea: false,
+            color: '#ebedef'
           }
         }
       }
